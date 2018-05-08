c1ec812bbfac539631f2c08c687d819658783284
Stanley <ggmunson756@gmail.com>
2018-05-07 20:40:53 -0600

    Created Local DB. Generated Dataset

--- HelpDesk/Program.cs
+++ HelpDesk/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace HelpDesk
 {
@@ -10,6 +11,7 @@ namespace HelpDesk
     {
         static void Main(string[] args)
         {
+            String cs = "";
         }
     }
 }
